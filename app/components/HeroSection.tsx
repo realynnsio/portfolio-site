a2e66cce03688bfa6dd586387abdb687ca745421
sion <n.siomusic@gmail.com>
2025-07-16 14:13:20 +0700

    fix: updated cv link

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -51,7 +51,7 @@ const HeroSection = () => {
           </p>
 
           <div className='flex flex-col md:flex-row items-start mt-6 space-y-2 md:space-y-0 md:space-x-4 rtl:space-x-reverse'>
-            <NavButton left={false} icon={<CVIcon />} className="outline-2 -outline-offset-1 outline-aqua-100 shadow-md w-fit hover:bg-aqua-100 hover:text-white" externalUrl="https://drive.google.com/file/d/1Dm2YUOt_L2f7HThGMECAZlXLTs8g4cf2/view?usp=sharing">View CV</NavButton>
+            <NavButton left={false} icon={<CVIcon />} className="outline-2 -outline-offset-1 outline-aqua-100 shadow-md w-fit hover:bg-aqua-100 hover:text-white" externalUrl="https://drive.google.com/file/d/1QaT6egsOcn9op_rVMQW4lr_5aHUwvDQk/view?usp=sharing">View CV</NavButton>
             <NavButton className="mt-2 md:mt-0 bg-aqua-100 text-white hover:bg-dive-100 md:hover:bg-dive-100 shadow-md hover:text-white w-fit" href="#contact">Contact Me</NavButton>
           </div>
         </div>
